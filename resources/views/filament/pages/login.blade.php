3acd2d03f693b1ab0bc34b323e370c1821ecf267
Calvin Jackson <amigohmccclurkin98@gmail.com>
2023-09-03 15:53:09 +0300

    chore: adding tsparticles script CDN

--- resources/views/filament/pages/login.blade.php
+++ resources/views/filament/pages/login.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-white dark:bg-gray-900 flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div class="mx-auto w-full max-w-sm lg:w-96">
             <div>
-                <h2 class="mt-6 text-3xl font-bold tracking-tight text-gray-900 dark:text-white/90">Sign in </h2>
+                <h2 class="mt-6 text-3xl font-bold tracking-tight text-gray-900 dark:text-white/90">Sign in</h2>
             </div>
 
             <div class="mt-8">
@@ -26,8 +26,19 @@
         </div>
     </div>
     <div class="relative hidden w-0 flex-1 lg:block">
-        <img class="absolute inset-0 h-full w-full object-cover" src="{{ asset('images/zally-orsi-DF3LkQN6qgo-unsplash.jpg') }}" alt="">
+        <div class="absolute inset-0 h-full w-full" id="particles"></div>
     </div>
 
     <x-filament-actions::modals />
+
+    <script src="https://cdn.jsdelivr.net/npm/tsparticles-preset-firefly@2/tsparticles.preset.firefly.bundle.min.js"></script>
+    <script>
+        (async () => {
+            await loadFireflyPreset(tsParticles); // this is required only if you are not using the bundle script
+
+            await tsParticles.load("particles", {
+                preset: "firefly",
+            });
+        })();
+    </script>
 </div>
